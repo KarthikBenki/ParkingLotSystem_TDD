1e18d3ad92fd9f3f925517ca3b22da331a1dd31a
KarthikBenki <karthikmc007@gmail.com>
2022-04-25 21:04:37 +0530

    Refactored ParkinglotSystem With comments

--- src/main/java/com/bridgelabz/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/ParkingLotSystem.java
@@ -1,11 +1,15 @@
 package com.bridgelabz;
-
+/**
+ * @author -> Karthik M C
+ * @since -> 25/04/2022
+ */
 public class ParkingLotSystem {
-
+  /** This program manages parking spaces for vehicles. */
   Vehicle vehicle = null; // intialising vehicle with null
 
-  /*
-  method to park the vehicle
+  /**
+   * @param vehicle -> Required to park the given vehicle.
+   * @return -> Returns boolean by checking if the given vehicle is already parked.
    */
   public boolean parkTheVehicle(Vehicle vehicle) {
     if (this.vehicle != null) // checking for empty slot
@@ -13,8 +17,10 @@ public class ParkingLotSystem {
     this.vehicle = vehicle; // parking the vehicle
     return true;
   }
-  /*
-  method to unpark the vehicle
+
+  /**
+   * @param vehicle -> Required to un-park the given vehicle.
+   * @return -> Returns boolean by checking if the given vehicle exists.
    */
   public boolean unParkTheVehicle(Vehicle vehicle) {
     if (this.vehicle == null) return false;
