b2915f9632d6b248fbfd66dbd39e9c9ec15b2757
KarthikBenki <karthikmc007@gmail.com>
2022-04-26 23:30:39 +0530

    Refactored park method using hash map

--- src/main/java/com/bridgelabz/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/ParkingLotSystem.java
@@ -1,36 +1,42 @@
 package com.bridgelabz;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author -> Karthik M C
  * @since -> 25/04/2022
  */
 public class ParkingLotSystem {
-  private static final int MAX_SIZE_OF_PARKINGLOT = 5;
+  private static final int MAX_SIZE_OF_PARKINGLOT = 2;
+  private Map<String, Vehicle> parkingLotMap = new HashMap<>();
   /** This program manages parking spaces for vehicles. */
   Vehicle vehicle = null; // intialising vehicle with null
 
   /**
-   *
    * @param vehicle
    * @throws ParkingLotException if parking lot is full
    */
   public void parkTheVehicle(Vehicle vehicle) throws ParkingLotException {
-    if (this.vehicle != null) // checking for empty slot
+    if (parkingLotMap.size() <= MAX_SIZE_OF_PARKINGLOT)
+      parkingLotMap.put(vehicle.id, vehicle);
+//    if (this.vehicle != null) // checking for empty slot
+    if(parkingLotMap.size()==MAX_SIZE_OF_PARKINGLOT)
     throw new ParkingLotException("Parking Lot is Full");
-    this.vehicle = vehicle; // parking the vehicle
+//    this.vehicle = vehicle; // parking the vehicle
   }
 
   /**
-   *
    * @param vehicle
    * @return true if vehicle is parked
    */
   public boolean isVehicleParked(Vehicle vehicle) {
-    if (this.vehicle.equals(vehicle)) return true;
+//    if (this.vehicle.equals(vehicle)) return true;
+    if(this.parkingLotMap.containsValue(vehicle)) return true;
     return false;
   }
 
   /**
-   *
    * @param vehicle
    * @throws ParkingLotException when parking lot is empty and asking for different vehicle
    */
@@ -44,7 +50,6 @@ public class ParkingLotSystem {
   }
 
   /**
-   *
    * @param vehicle
    * @return true if vehicle is unparked
    */
@@ -56,8 +61,4 @@ public class ParkingLotSystem {
   public boolean checkParkingLot(Vehicle[] vehicles) {
     return vehicles.length == MAX_SIZE_OF_PARKINGLOT;
   }
-
-
-
-
 }
