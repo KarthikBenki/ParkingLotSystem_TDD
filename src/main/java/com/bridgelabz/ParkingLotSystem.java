77748b26cca76e2912992b5fa67f73517c003b67
KarthikBenki <karthikmc007@gmail.com>
2022-04-27 00:28:36 +0530

    notifying parking lot full message to observers

--- src/main/java/com/bridgelabz/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/ParkingLotSystem.java
@@ -1,6 +1,8 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -10,6 +12,7 @@ import java.util.Map;
 public class ParkingLotSystem {
   private static final int MAX_SIZE_OF_PARKINGLOT = 2;
   private Map<String, Vehicle> parkingLotMap = new HashMap<>();
+  List<IParkingObserver> observers = new ArrayList<>();
   /** This program manages parking spaces for vehicles. */
   Vehicle vehicle = null; // intialising vehicle with null
 
@@ -20,10 +23,22 @@ public class ParkingLotSystem {
   public void parkTheVehicle(Vehicle vehicle) throws ParkingLotException {
     if (parkingLotMap.size() <= MAX_SIZE_OF_PARKINGLOT)
       parkingLotMap.put(vehicle.id, vehicle);
-//    if (this.vehicle != null) // checking for empty slot
-    if(parkingLotMap.size()==MAX_SIZE_OF_PARKINGLOT)
-    throw new ParkingLotException("Parking Lot is Full");
+    //    if (this.vehicle != null) // checking for empty slot
+    if (parkingLotMap.size() == MAX_SIZE_OF_PARKINGLOT) {
+      notifyObservers("Parking Lot is Full");
+      throw new ParkingLotException("Parking Lot is Full");
+    }
 //    this.vehicle = vehicle; // parking the vehicle
+  }
+
+  /**
+   *
+   * @param //updating message to owner and security
+   */
+  private void notifyObservers(String message) {
+    for(IParkingObserver list :observers){
+      list.update(message);
+    }
   }
 
   /**
